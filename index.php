93999e9d69f590b671630905ba57d281ffa5ae9c
navegantes <raphaelnavegantes@gmail.com>
2024-05-08 05:01:53 -0300

    refact query params

--- index.php
+++ index.php
@@ -7,8 +7,14 @@ require 'Database.php';
 $config = require('config.php');
 
 $db = new Database($config['database']);
-$posts = $db->query("SELECT * FROM posts")->fetchAll();
 
-foreach ($posts as $post) {
-  echo "<li>" . $post['title'] . "</li>";
-}
+$id = $_GET['id'];
+$query = "SELECT * FROM posts WHERE id =?";
+
+$posts = $db->query($query, [$id])->fetch();
+
+dd($posts);
+
+// foreach ($posts as $post) {
+//   echo "<li>" . $post['title'] . "</li>";
+// }
