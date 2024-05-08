5bf3894bcfa924748c35c96c3faa973dd78868a1
navegantes <raphaelnavegantes@gmail.com>
2024-05-08 03:42:31 -0300

    add database

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 
-require 'router.php';
+
+$db = new Database();
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['title'] . "</li>";
+}
