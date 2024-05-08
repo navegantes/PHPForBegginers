0c019868cabd27231fc802d8046bdbba11085418
navegantes <raphaelnavegantes@gmail.com>
2024-05-08 00:31:01 -0300

    add router and 404 files

--- index.php
+++ index.php
@@ -2,21 +2,4 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-
-$routes = [
-  '/' => 'controllers/index.php',
-  '/about' => 'controllers/about.php',
-  '/contact' => 'controllers/contact.php',
-];
-
-if (array_key_exists($uri, $routes)) {
-  require $routes[$uri];
-} else {
-  http_response_code(404);
-
-  echo "Page not found!";
-
-  die();
-}
+require 'router.php';
